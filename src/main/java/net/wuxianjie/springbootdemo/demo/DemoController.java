fa5054fa3cdba512633470a4a2bf2bcffe9e7b90
吴仙杰 <wuxianjiezh@gmail.com>
2023-08-21 10:23:25 +0800

    增加环境变量

--- src/main/java/net/wuxianjie/springbootdemo/demo/DemoController.java
+++ src/main/java/net/wuxianjie/springbootdemo/demo/DemoController.java
@@ -1,18 +1,27 @@
 package net.wuxianjie.springbootdemo.demo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class DemoController {
+
+  public static final String ENV_NAME = "NAME";
+
+  private final Environment environment;
 
   @GetMapping("/version")
   public Version getVersion() {
-    return new Version("Spring Boot Demo", "v0.0.1", LocalDateTime.now());
+    String name = Optional.ofNullable(environment.getProperty(ENV_NAME)).orElse("Spring Boot Demo");
+    return new Version(name, "v0.0.1", LocalDateTime.now());
   }
 
   record Version(String name, String version, LocalDateTime now) {}
