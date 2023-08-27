fcad140eff4c96c851cbd877a798bd39e8c19a1a
吴仙杰 <wuxianjiezh@gmail.com>
2023-08-27 17:39:07 +0800

    set ex

--- src/main/java/net/wuxianjie/springbootdemo/redis/RedisController.java
+++ src/main/java/net/wuxianjie/springbootdemo/redis/RedisController.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -14,9 +16,13 @@ public class RedisController implements CommandLineRunner {
   private final RedisTemplate<String, String> redisTemplate;
 
   @Override
-  public void run(String... args) {
-    redisTemplate.opsForValue().setIfAbsent("color", "hi there");
+  public void run(String... args) throws InterruptedException {
+    redisTemplate.opsForValue().set("color", "red", 2, TimeUnit.SECONDS);
     String color = redisTemplate.opsForValue().get("color");
     log.info("color: {}", color);
+
+    TimeUnit.SECONDS.sleep(2);
+    color = redisTemplate.opsForValue().get("color");
+    log.info("color after 2 seconds: {}", color);
   }
 }
