a1123d57c6bbaedc42d8a2dccb34274cc5d80f04
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-02 17:52:49 +0800

    根据用户 id 获取用户数据

--- src/main/java/net/wuxianjie/springbootdemo/redis/UserController.java
+++ src/main/java/net/wuxianjie/springbootdemo/redis/UserController.java
@@ -1,23 +1,22 @@
 package net.wuxianjie.springbootdemo.redis;
 
-import cn.hutool.core.lang.Console;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.wuxianjie.springbootdemo.shared.exception.ApiException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -26,9 +25,20 @@ public class UserController {
   private final StringRedisTemplate redisTemplate;
   private final ObjectMapper objectMapper;
 
+  @GetMapping("/users/{userId}")
+  public ResponseEntity<User> getUser(@PathVariable String userId) {
+    Optional<User> userOpt = getUserFromRedis(userId);
+    if (userOpt.isEmpty()) {
+      throw new ApiException(HttpStatus.NOT_FOUND, "用户不存在");
+    }
+
+    return ResponseEntity.ok(userOpt.get());
+  }
+
   @PostMapping("/users")
   public ResponseEntity<Void> createUser(@Valid @RequestBody NewUser user) {
-    String key = KeyUtils.getUsers(user.username());
+    String userId = KeyUtils.getUuid();
+    String key = KeyUtils.getUsers(userId);
 
     checkForUserNotExists(key);
 
@@ -55,13 +65,25 @@ public class UserController {
   }
 
   private CachedUser createCachedUser(NewUser user) {
-    return new CachedUser(KeyUtils.getUuid(), user.password(), LocalDateTime.now());
+    return new CachedUser(user.username(), user.password(), LocalDateTime.now());
   }
 
   private void printSavedUser(String key) {
     Map<Object, Object> entries = redisTemplate.opsForHash().entries(key);
     CachedUser cachedUser = objectMapper.convertValue(entries, CachedUser.class);
-    Console.log(cachedUser);
+    log.info("Data in redis: {}", cachedUser);
+  }
+
+  private Optional<User> getUserFromRedis(String userId) {
+    String key = KeyUtils.getUsers(userId);
+    Map<Object, Object> val = redisTemplate.opsForHash().entries(key);
+    if (val.isEmpty()) {
+      return Optional.empty();
+    }
+
+    CachedUser cachedUser = objectMapper.convertValue(val, CachedUser.class);
+
+    return Optional.of(new User(userId, cachedUser.username(), cachedUser.password(), cachedUser.createAt()));
   }
 }
 
@@ -72,4 +94,6 @@ record NewUser (
   String password
 ) {}
 
-record CachedUser (String id, String password, LocalDateTime createAt) {}
+record CachedUser (String username, String password, LocalDateTime createAt) {}
+
+record User (String id, String username, String password, LocalDateTime createdAt) {}
