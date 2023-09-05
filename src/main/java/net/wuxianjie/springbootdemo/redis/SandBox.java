7935de172728678efc006ff995ff9e3bbe1c961b
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-05 14:53:49 +0800

    sorted sort: zrange

--- src/main/java/net/wuxianjie/springbootdemo/redis/SandBox.java
+++ src/main/java/net/wuxianjie/springbootdemo/redis/SandBox.java
@@ -8,7 +8,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
-import java.util.Set;
+import java.io.Serializable;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
@@ -31,26 +32,19 @@ public class SandBox implements CommandLineRunner {
     add = redisTemplate.opsForZSet().add("pencilbox", "eraser", 0.5);
     Console.log("zadd pencilbox 0.5 eraser --> {}", add);
 
-    // zpopmin pencilbox 2
-    Set<ZSetOperations.TypedTuple<String>> min2 = redisTemplate.opsForZSet().popMin("pencilbox", 4);
-    if (min2 == null) {
-      Console.log("zpopmin pencilbox 2 --> null");
+    // zrange pencilbox 1 2 withscores
+    Set<ZSetOperations.TypedTuple<String>> pencilbox = redisTemplate.opsForZSet().rangeWithScores("pencilbox", 1, 2);
+    if (pencilbox == null) {
+      Console.log("zrange pencilbox 1 2 withscores ---> null");
     } else {
-      min2.forEach(stringTypedTuple -> {
-        String value = stringTypedTuple.getValue();
-        Double score = stringTypedTuple.getScore();
-        Console.log("zpopmin pencilbox 2 --> {}: {}", value, score);
-      });
-    }
-
-    // zpopmax pencilbox
-    ZSetOperations.TypedTuple<String> max = redisTemplate.opsForZSet().popMax("pencilbox");
-    if (max == null) {
-      Console.log("zpopmax pencilbox --> null");
-    } else {
-      String value = max.getValue();
-      Double score = max.getScore();
-      Console.log("zpopmax pencilbox --> {}: {}", value, score);
+      List<LinkedHashMap<String, Object>> list = pencilbox.stream().map(i -> {
+          LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+          map.put("memember", i.getValue());
+          map.put("score", i.getScore());
+          return map;
+        })
+        .toList();
+      Console.log("zrange pencilbox 1 2 withscores ---> {}", list);
     }
   }
 }
