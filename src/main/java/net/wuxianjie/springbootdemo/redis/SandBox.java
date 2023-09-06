7364ba4f363346df7be4beff843165ba142ff715
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-06 15:54:56 +0800

    模拟商品查看数

--- src/main/java/net/wuxianjie/springbootdemo/redis/SandBox.java
+++ src/main/java/net/wuxianjie/springbootdemo/redis/SandBox.java
@@ -8,13 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.connection.StringRedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -25,65 +22,101 @@ public class SandBox implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    // 模拟查找最贵的商品
-    // 构造假商品数据
+    // 模拟商品查看数
+    // 商品数据
     List<Product> products = List.of(
-      new Product(1L, "iPhone", 50),
-      new Product(2L, "iPods", 20),
-      new Product(3L, "MacBook", 80)
+      new Product(1L, "iPhone", 0),
+      new Product(2L, "iPod", 0),
+      new Product(3L, "MacBook", 0)
     );
 
-    // 存入商品数据
+    // 将商品数据存入 Redis hash
     saveProducts(products);
 
-    // 存入价格
-    saveProductPrices(products);
+    // 模拟查看商品存入 Redis zSet
+    viewProduct("wxj");
 
-    // 获取最贵的商品
-    Product highestPriceProduct = getHighestPriceProduct();
-    Console.log("最贵商品: {}", highestPriceProduct);
+    // 同一用户多次查看一个商品仅算一次
+    viewProduct("wxj");
+
+    // 其他用户查看再增加一次
+    viewProduct("jason");
+
+    // 查看商品详细
+    showProductDetails();
   }
 
-  private Product getHighestPriceProduct() {
-    Set<ZSetOperations.TypedTuple<String>> set = redisTemplate.opsForZSet().reverseRangeByScoreWithScores("products:price", 0, Long.MAX_VALUE, 0, 1);
-    if (set == null) {
-      throw new IllegalArgumentException("商品价格缓存不存在");
+  private void showProductDetails() {
+    Product product = new Product(1L, "iPhone", 0);
+
+    Map<Object, Object> entries = redisTemplate.opsForHash().entries("products:" + product.id());
+    Console.log("Product Info: {}", entries);
+  }
+
+  private void viewProduct(String username) {
+    Product product = new Product(1L, "iPhone", 0);
+
+    String key = "views:" + username;
+
+    Double viewedNums = redisTemplate.opsForZSet().score(key, product.id() + "");
+    if (viewedNums == null) {
+      redisTemplate.opsForZSet().add(key, product.id() + "", 1);
+      incrementViews(product.id());
+      return;
     }
 
-    ZSetOperations.TypedTuple<String> highestPriceProduct = set.stream().findFirst().orElseThrow();
-    long productId = Long.parseLong(Objects.requireNonNull(highestPriceProduct.getValue()));
+    if (viewedNums > 0) {
+      return;
+    }
 
-    return getProductById(productId);
+    redisTemplate.opsForZSet().incrementScore(key, product.id() + "", 1);
   }
 
-  private Product getProductById(long productId) {
-    Map<Object, Object> entries = redisTemplate.opsForHash().entries("products:" + productId);
-    return objectMapper.convertValue(entries, Product.class);
-  }
-
-  private void saveProductPrices(List<Product> products) {
-    redisTemplate.executePipelined((RedisCallback<?>) connection -> {
-      StringRedisConnection conn = (StringRedisConnection) connection;
-
-      products.forEach(product -> conn.zAdd("products:price", product.price(), product.id() + ""));
-
-      return null;
-    });
+  private void incrementViews(long productId) {
+    redisTemplate.opsForHash().increment("products:" + productId, "views", 1);
   }
 
   private void saveProducts(List<Product> products) {
+    if (isDataExists(products)) {
+      return;
+    }
+
     redisTemplate.executePipelined((RedisCallback<?>) connection -> {
       StringRedisConnection conn = (StringRedisConnection) connection;
 
       products.forEach(product -> {
-        Map<String, String> data = objectMapper.convertValue(product, new TypeReference<>() {});
+        Map<String, String> map = objectMapper.convertValue(product, new TypeReference<>() {});
 
-        conn.hMSet("products:" + product.id(), data);
+        conn.hMSet("products:" + product.id(), map);
       });
 
       return null;
     });
   }
+
+  private boolean isDataExists(List<Product> products) {
+    List<Boolean> booleans = isExistsProducts(products);
+
+    for (Object bool : booleans) {
+      if (Boolean.TRUE == bool) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private List<Boolean> isExistsProducts(List<Product> products) {
+    List<Object> booleans = redisTemplate.executePipelined((RedisCallback<?>) connection -> {
+      StringRedisConnection conn = (StringRedisConnection) connection;
+
+      products.forEach(product -> conn.hExists("products:" + product.id(), "views"));
+
+      return null;
+    });
+
+    return booleans.stream().map(o -> (Boolean) o).toList();
+  }
 }
 
-record Product(Long id, String name, double price) {}
+record Product(Long id, String name, int views) {}
