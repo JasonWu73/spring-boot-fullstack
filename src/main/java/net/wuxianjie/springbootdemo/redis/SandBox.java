5f9ea2a9c512b480eb7ef1feb4a15adeb442e4c7
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-08 17:45:14 +0800

    增加对比

--- src/main/java/net/wuxianjie/springbootdemo/redis/SandBox.java
+++ src/main/java/net/wuxianjie/springbootdemo/redis/SandBox.java
@@ -4,6 +4,8 @@ import cn.hutool.core.lang.Console;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -22,6 +24,7 @@ public class SandBox implements CommandLineRunner {
     // 模拟多线程不安全的情况
     new Thread(this::usingTransaction).start();
     new Thread(this::usingTransaction).start();
+    addMultiItem();
   }
 
   private void usingTransaction() {
@@ -57,5 +60,20 @@ public class SandBox implements CommandLineRunner {
   private boolean isEmpty() {
     Long size = redisTemplate.opsForList().size("list");
     return size == null || size == 0;
+  }
+
+  private void addMultiItem() {
+    List<Object> results = redisTemplate.executePipelined((RedisCallback<?>) connection -> {
+      StringRedisConnection conn = (StringRedisConnection) connection;
+
+      conn.listCommands().rPush("nums".getBytes(), "1".getBytes());
+      conn.listCommands().rPush("nums".getBytes(), "2".getBytes());
+      conn.listCommands().rPush("nums".getBytes(), "3".getBytes());
+      conn.listCommands().rPush("nums".getBytes(), "4".getBytes());
+
+      return null;
+    });
+
+    Console.log("results: {}", results);
   }
 }
