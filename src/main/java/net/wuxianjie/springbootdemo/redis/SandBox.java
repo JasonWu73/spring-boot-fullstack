67611736aa64d191000d2456af062bd031747565
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-09 16:35:46 +0800

    执行 LUA 脚本

--- src/main/java/net/wuxianjie/springbootdemo/redis/SandBox.java
+++ src/main/java/net/wuxianjie/springbootdemo/redis/SandBox.java
@@ -3,12 +3,8 @@ package net.wuxianjie.springbootdemo.redis;
 import cn.hutool.core.lang.Console;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.StringRedisConnection;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -21,59 +17,24 @@ public class SandBox implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    // 模拟多线程不安全的情况
-    new Thread(this::usingTransaction).start();
-    new Thread(this::usingTransaction).start();
-    addMultiItem();
-  }
+    // 运行 LUA 脚本
+    DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+    script.setScriptText("""
+      local key = KEYS[1]
+      local num = tonumber(ARGV[1])
+            
+      local val = redis.call('GET', key)
+      val = tonumber(val)
+      if val == nil then
+        val = 0
+      end
+            
+      val = val + num
+      redis.call('SET', key, val)
+      return val""");
+    script.setResultType(Long.class);
 
-  private void usingTransaction() {
-    List<Object> txResults = redisTemplate.execute(new SessionCallback<>() {
-      @Override
-      public List<Object> execute(RedisOperations operations) throws DataAccessException {
-        operations.watch("list");
-
-        operations.multi();
-
-        addItem();
-
-        return operations.exec();
-      }
-    });
-
-    Console.log("txResults: {}", txResults);
-  }
-
-  private void currencyIssue() {
-    boolean isEmpty = isEmpty();
-    if (!isEmpty) {
-      return;
-    }
-
-    addItem();
-  }
-
-  private void addItem() {
-    redisTemplate.opsForList().rightPush("list", "a");
-  }
-
-  private boolean isEmpty() {
-    Long size = redisTemplate.opsForList().size("list");
-    return size == null || size == 0;
-  }
-
-  private void addMultiItem() {
-    List<Object> results = redisTemplate.executePipelined((RedisCallback<?>) connection -> {
-      StringRedisConnection conn = (StringRedisConnection) connection;
-
-      conn.listCommands().rPush("nums".getBytes(), "1".getBytes());
-      conn.listCommands().rPush("nums".getBytes(), "2".getBytes());
-      conn.listCommands().rPush("nums".getBytes(), "3".getBytes());
-      conn.listCommands().rPush("nums".getBytes(), "4".getBytes());
-
-      return null;
-    });
-
-    Console.log("results: {}", results);
+    Long result = redisTemplate.execute(script, List.of("books:count"), "10");
+    System.out.printf("Result: %s%n", result);
   }
 }
