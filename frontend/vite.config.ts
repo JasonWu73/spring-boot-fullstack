f717d9ebbc8b2278e263d3745a7c5a1d5b59ed9d
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-15 18:19:24 +0800

    配置别名

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    // 配置别名
+    alias: {
+      '@': '/src'
+    }
+  }
 });
