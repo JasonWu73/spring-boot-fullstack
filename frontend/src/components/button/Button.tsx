e80f63f115cf13aec44236fd02e8da51362d100f
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-15 11:10:55 +0800

    增加 Button 组件配置

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 
 type ButtonProps = {
-  label: string,
-  onClick?: React.MouseEventHandler<HTMLButtonElement>,
-  type?: 'button' | 'submit' | 'reset',
-  disabled?: boolean,
-  className?: string
+  label: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  btn?: 'primary' | 'danger' | 'light';
+  type?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
+  className?: string;
 };
 
-export default function Button({ label, onClick, type = 'button', disabled, className }: ButtonProps) {
-  const defaultClassName = 'px-4 py-2 text-sm bg-sky-500 text-white rounded shadow-sm hover:bg-sky-600 active:bg-sky-700 focus:outline-none focus:ring focus:ring-sky-300 disabled:opacity-50 disabled:cursor-not-allowed';
+export default function Button({ label, onClick, btn = 'primary', type = 'button', disabled, className }: ButtonProps) {
+  const btnClass = getBtnClass(btn);
 
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`${defaultClassName} ${className}`}
+      className={`px-4 py-2 text-sm rounded shadow-sm focus:outline-none focus:ring disabled:opacity-50 disabled:cursor-not-allowed ${btnClass} ${className}`}
     >
       {label}
     </button>
   );
+}
+
+function getBtnClass(btn: ButtonProps['btn']) {
+  switch (btn) {
+    case 'danger':
+      return 'bg-red-500 text-white hover:bg-red-600 active:bg-red-700 focus:ring-red-300';
+    case 'light':
+      return 'bg-slate-200 text-black hover:bg-slate-300 active:bg-slate-400 focus:ring-slate-100';
+    default:
+      return 'bg-sky-500 text-white hover:bg-sky-600 active:bg-sky-700 focus:ring-sky-300';
+  }
 }
