788ae0ac9f4bc994b274d432795aca5d2b642967
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-16 09:19:38 +0800

    初始化

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import ProductDemo from '@/components/demo/ProductDemo.tsx';
-
 /**
  * 呈现主要应用程序组件。
  *
@@ -9,8 +7,6 @@ export default function App() {
   return (
     <div className="mt-8 mx-8 p-4 rounded border shadow-sm">
       <h1 className="font-bold text-4xl underline">Hello World!</h1>
-
-      <ProductDemo />
     </div>
   );
 }
