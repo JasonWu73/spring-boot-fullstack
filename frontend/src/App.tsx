d96d1cafa60cad386ccc889d4c19b968d5f60a32
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-16 09:37:16 +0800

    体验非 JSX API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,29 @@
+import React from 'react';
+
 /**
  * 呈现主要应用程序组件。
  *
  * @return 呈现的应用程序组件
  */
 export default function App() {
-  return (
-    <div className="mt-8 mx-8 p-4 rounded border shadow-sm">
-      <h1 className="font-bold text-4xl underline">Hello World!</h1>
-    </div>
-  );
+  // const time = new Date().toLocaleTimeString();
+  const [time, setTime] = React.useState(new Date().toLocaleTimeString());
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return React.createElement("header", null, '你好 React: ' + time);
+
+  // return (
+  //   <div className="mt-8 mx-8 p-4 rounded border shadow-sm">
+  //     <h1 className="font-bold text-4xl underline">Hello World!</h1>
+  //   </div>
+  // );
 }
