a4556d46e338dd79534b3e16a4583b4ab91716d7
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-17 11:56:35 +0800

    练习 - 可重用组件

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import ProductShowcase from '@/components/demo/ProductShowcase.tsx';
+import Pizza from '@/components/pizzas/Pizza.tsx';
 
 export default function App() {
   return (
     <div className="mt-8 mx-8 p-4 rounded border shadow-sm">
       <h1 className="font-bold text-4xl underline">Hello World!</h1>
 
-      <ProductShowcase />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
     </div>
   );
 }
