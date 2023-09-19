bc164e79472e388a7fc84dbb8cd93c037fcd6dac
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-19 14:21:36 +0800

    auto change color theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,43 @@
 import Draft from "@/components/drafts/Draft.tsx";
+import { useEffect } from "react";
 
 export default function App() {
+  useTheme();
+
   return (
     <Draft />
   );
+}
+
+function useTheme() {
+  useEffect(() => {
+    // 监听暗色模式变化
+    const mediaQuery = matchMedia("(prefers-color-scheme: dark)");
+
+    // 初始化
+    mediaQuery.matches && addDarkBodyClass();
+
+    function handleDarkMode(e: MediaQueryListEvent) {
+      if (e.matches) {
+        addDarkBodyClass();
+        return;
+      }
+
+      removeDarkBodyClass();
+    }
+
+    mediaQuery.addEventListener("change", handleDarkMode);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleDarkMode);
+    };
+  }, []);
+}
+
+function addDarkBodyClass() {
+  document.body.classList.add("dark");
+}
+
+function removeDarkBodyClass() {
+  document.body.classList.remove("dark");
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "nord": {
+          "200": "#ECEFF4",
+          "300": "#E5E9F0",
+          "400": "#D8DEE9",
+          "500": "#2E3440",
+          "600": "#3B4252",
+          "700": "#434C5E",
+          "800": "#4C566A"
+        }
+      }
+    }
   },
   plugins: []
 };
