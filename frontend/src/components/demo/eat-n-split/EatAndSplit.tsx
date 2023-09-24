1955acfcf3917e0fc111f722d889c7cdb428dd31
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-24 12:48:03 +0800

    增加用户

--- frontend/src/components/demo/eat-n-split/EatAndSplit.tsx
+++ frontend/src/components/demo/eat-n-split/EatAndSplit.tsx
@@ -22,6 +22,30 @@ const initialFriends = [
     name: "Lily",
     image: "https://i.pravatar.cc/400?img=24",
     balance: 0
+  },
+  {
+    id: 4,
+    name: "Linda",
+    image: "https://i.pravatar.cc/400?img=23",
+    balance: 0
+  },
+  {
+    id: 5,
+    name: "Luna",
+    image: "https://i.pravatar.cc/400?img=22",
+    balance: 0
+  },
+  {
+    id: 6,
+    name: "Lola",
+    image: "https://i.pravatar.cc/400?img=21",
+    balance: 0
+  },
+  {
+    id: 7,
+    name: "Lara",
+    image: "https://i.pravatar.cc/400?img=20",
+    balance: 0
   }
 ];
 
