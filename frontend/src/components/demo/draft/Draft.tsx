0ac5bf7dd466c13cbb423e40068c0caeb664527c
吴仙杰 <wuxianjiezh@gmail.com>
2023-09-28 17:57:34 +0800

    useEffect

--- frontend/src/components/demo/draft/Draft.tsx
+++ frontend/src/components/demo/draft/Draft.tsx
@@ -1,125 +1,49 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/Tabs'
-import { Card, CardContent, CardFooter, CardTitle } from '@/components/ui/Card'
-import { Button } from '@/components/ui/Button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { sendRequest } from '@/lib/http'
 
-const list = [
-  {
-    id: 1,
-    name: 'Tab 1',
-    content: 'Tab 1 Content: Agree focus commercial prove.',
-    likes: 0
-  },
-  {
-    id: 2,
-    name: 'Tab 2',
-    content: 'Tab 2 Content: Receive system that reach most continue.',
-    likes: 0
-  },
-  {
-    id: 3,
-    name: 'Tab 3',
-    content: 'Tab 3 Content: Suggest meat final.',
-    likes: 0
-  },
-  {
-    id: 4,
-    name: 'Tab 4',
-    content: 'Tab 4 Content: Animal artist imagine forward wish actually.',
-    likes: 0
-  }
-]
-
-export default function Draft() {
-  return (
-    <Tabs
-      defaultValue={`tab${list[0].id}`}
-      className="mx-auto mt-8 flex w-[400px] flex-col items-center rounded border border-amber-200 p-4 dark:bg-slate-700"
-    >
-      <TabsList>
-        {list.map(({ id, name }) => (
-          <TabsTrigger key={id} value={`tab${id}`}>
-            {name}
-          </TabsTrigger>
-        ))}
-      </TabsList>
-
-      <div className="w-full">
-        {list.map((item) => (
-          <TabContent key={item.id} item={item} />
-        ))}
-      </div>
-    </Tabs>
-  )
+type Product = {
+  id: number
+  title: string
 }
 
-type TabContentProps = { item: (typeof list)[0] }
+type ProductsResponse = {
+  products: Product[]
+}
 
-function TabContent({
-  item: { id, name, content, likes: initialLikes }
-}: TabContentProps) {
-  const [likes, setLikes] = useState(initialLikes)
-  const [showContent, setShowContent] = useState(true)
+export default function Draft() {
+  const [products, setProducts] = useState<Product[]>([])
 
-  console.log('render')
+  useEffect(() => {
+    const controller = new AbortController()
 
-  function handleUndo() {
-    setLikes(0)
-    setShowContent(true)
-  }
+    async function fetchData() {
+      const { data, error } = await sendRequest<ProductsResponse, string>({
+        url: 'https://dummyjson.com/products/search?q=phone',
+        signal: controller.signal
+      })
 
-  function handleDelayUndo() {
-    setTimeout(handleUndo, 2000)
-  }
+      if (error) {
+        console.log(error)
+        return
+      }
 
-  function handleLike() {
-    setLikes((prev) => prev + 1)
-  }
+      data && setProducts(data.products)
+    }
 
-  function handleTripleLikes() {
-    handleLike()
-    handleLike()
-    handleLike()
-  }
+    fetchData().then()
+
+    return () => {
+      controller.abort()
+    }
+  }, [])
 
   return (
-    <TabsContent value={`tab${id}`}>
-      <Card className="flex flex-col items-center justify-between gap-4 pt-4 dark:bg-slate-600">
-        <CardTitle>{name}</CardTitle>
-
-        <CardContent className="w-full">
-          {showContent && <div>{content}</div>}
-
-          <div className="flex justify-between">
-            <Button
-              onClick={() => setShowContent((prev) => !prev)}
-              variant="link"
-              className="px-0 text-sky-500"
-            >
-              {showContent ? 'Hide Content' : 'Show Content'}
-            </Button>
-
-            <div className="flex items-center gap-2">
-              <span>{likes} ❤️</span>
-              <Button onClick={handleLike} variant="destructive" size="sm">
-                +
-              </Button>
-              <Button
-                onClick={handleTripleLikes}
-                variant="destructive"
-                size="sm"
-              >
-                +++
-              </Button>
-            </div>
-          </div>
-        </CardContent>
-
-        <CardFooter className="flex w-full justify-start gap-2">
-          <Button onClick={handleUndo}>Undo</Button>
-          <Button onClick={handleDelayUndo}>Undo in 2s</Button>
-        </CardFooter>
-      </Card>
-    </TabsContent>
+    <div className="mx-auto mt-8 w-96 rounded border border-amber-500 p-4">
+      <ul>
+        {products.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
