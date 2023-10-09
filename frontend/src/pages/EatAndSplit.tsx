6295f11e0ca8ad45345fe31f22a91f9e41c864db
吴仙杰 <wuxianjiezh@gmail.com>
2023-10-09 14:46:16 +0800

    去除 Grid 两行的定义, 第二行采用默认策略

--- frontend/src/pages/EatAndSplit.tsx
+++ frontend/src/pages/EatAndSplit.tsx
@@ -122,7 +122,7 @@ function EatAndSplit() {
   }
 
   return (
-    <div className="grid grid-flow-row items-center justify-center gap-6 p-4 md:mt-6 md:grid-cols-2 md:grid-rows-2">
+    <div className="grid grid-flow-row items-center justify-center gap-6 p-4 md:mt-6 md:grid-cols-2">
       <div className="md:col-span-1 md:row-span-1 md:justify-self-end">
         <Input
           value={search}
