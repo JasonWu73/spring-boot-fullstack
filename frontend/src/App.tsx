785e08d9efc4840f969ef3404dc07a83674789eb
吴仙杰 <wuxianjiezh@gmail.com>
2023-10-14 20:17:29 +0800

    React Router 下的懒加载 (与正常 React 无异)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 import { ThemeProvider } from '@/components/ui/ThemeProvider'
 import { Toaster } from '@/components/ui/Toaster'
 import { NavBar } from '@/components/NavBar'
-import { FormSplitBill } from '@/components/eat-n-split/FormSplitBill'
 import { Login } from '@/pages/Login'
 import { ProductShowcase } from '@/pages/ProductShowcase'
 import { EatAndSplit } from '@/pages/EatAndSplit'
 import { PageNotFound } from '@/pages/PageNotFound'
+import { wait } from '@/lib/utils'
+import { Loading } from '@/components/ui/Loading'
+
+// ----- Start: 测试 React Router 懒加载 (React Split Code 技术) -----
+const FormSplitBill = lazy(() =>
+  wait(1).then(() =>
+    import('@/components/eat-n-split/FormSplitBill').then((module) => ({
+      default: module.FormSplitBill
+    }))
+  )
+)
+// ----- End: 测试 React Router 懒加载 (React Split Code 技术) -----
 
 export default function App() {
   return (
@@ -20,7 +32,14 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/fetch" element={<ProductShowcase />} />
           <Route path="/eat-split" element={<EatAndSplit />}>
-            <Route path=":friendId" element={<FormSplitBill />} />
+            <Route
+              path=":friendId"
+              element={
+                <Suspense fallback={<Loading />}>
+                  <FormSplitBill />
+                </Suspense>
+              }
+            />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
