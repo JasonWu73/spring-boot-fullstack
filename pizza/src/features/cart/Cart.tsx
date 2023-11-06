d6485aa0435578b10fd7f0e80d3bf321f15ae781
吴仙杰 <wuxianjiezh@gmail.com>
2023-11-06 18:51:50 +0800

    创建布局路由

--- pizza/src/features/cart/Cart.tsx
+++ pizza/src/features/cart/Cart.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 const fakeCart = [
   {
@@ -6,39 +6,39 @@ const fakeCart = [
     name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
-    totalPrice: 32,
+    totalPrice: 32
   },
   {
     pizzaId: 6,
     name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
-    totalPrice: 13,
+    totalPrice: 13
   },
   {
     pizzaId: 11,
     name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+    totalPrice: 15
+  }
+]
 
 function Cart() {
-  const cart = fakeCart;
+  const cart = fakeCart
 
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <Link to="/menu">← 返回菜单</Link>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2>您的购物车，%NAME%</h2>
 
       <div>
-        <Link to="/order/new">Order pizzas</Link>
-        <button>Clear cart</button>
+        <Link to="/order/new">订购披萨</Link>
+        <button>清除购物车</button>
       </div>
     </div>
-  );
+  )
 }
 
-export default Cart;
+export default Cart
--- pizza/src/ui/layout/AppLayout.tsx
+++ pizza/src/ui/layout/AppLayout.tsx
@@ -1,9 +1,19 @@
+import { Outlet } from 'react-router-dom'
+
 import { Header } from '@/ui/layout/Header'
+import CartOverview from '@/features/cart/CartOverview'
 
 function AppLayout() {
   return (
     <div>
       <Header />
+
+      <main>
+        <h1>内容</h1>
+        <Outlet />
+      </main>
+
+      <CartOverview />
     </div>
   )
 }
