d8441a539461d210b392de0de27c84539e520d7d
吴仙杰 <wuxianjiezh@gmail.com>
2023-11-21 00:23:16 +0800

    选中行的提示使用 toast

--- frontend/src/user/UserListPage.tsx
+++ frontend/src/user/UserListPage.tsx
@@ -15,6 +15,7 @@ import {
   DEFAULT_PAGE_SIZE,
   type Paging
 } from '@/shared/components/ui/DataTable'
+import { useToast } from '@/shared/components/ui/use-toast'
 import { useFetch } from '@/shared/hooks/use-fetch'
 import { useRefresh } from '@/shared/hooks/use-router'
 import { useTitle } from '@/shared/hooks/use-title'
@@ -50,6 +51,8 @@ export default function UserListPage() {
   const authority = searchParams.get('authority')
 
   const { requestApi } = useAuth()
+
+  const { toast } = useToast()
 
   const {
     data,
@@ -96,9 +99,19 @@ export default function UserListPage() {
       .filter((_, index) => indexes.includes(index))
       .map((user) => user.id)
 
-    if (ids.length === 0) return alert('没有选中任何一行')
+    if (ids.length === 0) {
+      toast({
+        title: '未选中任何行',
+        description: '请先选中要操作的行',
+        variant: 'destructive'
+      })
+      return
+    }
 
-    alert(`被选中的行 ID(s)：${ids.join(', ')}`)
+    toast({
+      title: '选中的行',
+      description: `被选中的行 ID(s)：${ids.join(', ')}`
+    })
   }
 
   return (
