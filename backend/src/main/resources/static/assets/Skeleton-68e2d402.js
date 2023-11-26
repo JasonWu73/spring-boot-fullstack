c0b76bf2c7be2b89b173ffe913365ec2c47ec2b9
吴仙杰 <wuxianjiezh@gmail.com>
2023-11-26 12:15:55 +0800

    更新前端资源

---
+++ backend/src/main/resources/static/assets/Skeleton-68e2d402.js
@@ -0,0 +1,1 @@
+import{j as a,g as t}from"./index-990c888e.js";function r({className:e,...s}){return a.jsx("div",{className:t("animate-pulse rounded-md bg-slate-900/10 dark:bg-slate-500/50",e),...s})}export{r as S};
---
+++ backend/src/main/resources/static/assets/use-title-28640cc3.js
@@ -0,0 +1,1 @@
+import{e as o}from"./index-990c888e.js";function c(e){o.useEffect(()=>{const t=document.title;return e&&(document.title=e),()=>{document.title=t}},[e])}export{c as u};
