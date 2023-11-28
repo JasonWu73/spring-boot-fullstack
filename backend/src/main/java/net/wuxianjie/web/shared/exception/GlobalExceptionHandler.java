8c3f3da575d5ea21bb4893faab6a73f487227c06
吴仙杰 <wuxianjiezh@gmail.com>
2023-11-28 22:11:17 +0800

    更新简单的 SPA 处理

--- backend/src/main/java/net/wuxianjie/web/shared/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/net/wuxianjie/web/shared/exception/GlobalExceptionHandler.java
@@ -3,13 +3,18 @@ package net.wuxianjie.web.shared.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.ClientAbortException;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -20,12 +25,77 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 @Slf4j
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+  private static final String API_PATH_PREFIX = "/api/";
+  public static final String SPA_INDEX_PAGE = "classpath:/static/index.html";
+
+  private final ResourceLoader resourceLoader;
+
+  /**
+   * 单页应用（SPA）处理，按规则返回 JSON 数据或页面。
+   *
+   * <p>由 {@link DefaultHandlerExceptionResolver} 可知 Spring 中默认对应 404 的异常有哪些。
+   *
+   * <p>返回 JSON 数据：
+   *
+   * <ol>
+   *   <li>请求头 {@value HttpHeaders#ACCEPT} 中存在 {@value MediaType#APPLICATION_JSON_VALUE}</li>
+   *   <li>请求 URI 以 {@value #API_PATH_PREFIX} 开头</li>
+   * </ol>
+   *
+   * <p>其他情况一律返回页面，因为前端 SPA 单页面应用已作为静态资源打包在了 Jar 中。
+   *
+   * <p>Spring Boot 默认会将 {@code src/main/resources/static/} 中的内容作为 Web 静态资源提供。
+   *
+   * <p>约定 SPA 的页面入口：{@value #SPA_INDEX_PAGE}。
+   *
+   * <p><h2>扩展说明
+   *
+   * <p>Spring Boot Web 静态资源查找目录，由优先级高到低排序：
+   *
+   * <ol>
+   *   <li>{@code src/main/resources/META-INF/resources/}</li>
+   *   <li>{@code src/main/resources/resources/}</li>
+   *   <li>{@code src/main/resources/static/}</li>
+   *   <li>{@code src/main/resources/public/}</li>
+   * </ol>
+   *
+   * @return JSON 数据或 SPA 首页
+   */
+  @ExceptionHandler({NoResourceFoundException.class, NoHandlerFoundException.class})
+  public ResponseEntity<?> handleNoResourceFoundException(
+      final HttpServletRequest request
+  ) {
+    // 若为 API 或 JSON 请求，则返回 JSON 数据
+    final String requestPath = request.getRequestURI();
+    final String accept = request.getHeader(HttpHeaders.ACCEPT);
+
+    if (isJsonRequest(requestPath, accept)) {
+      return ResponseEntity
+          .status(HttpStatus.NOT_FOUND)
+          .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+          .body(new ApiError(HttpStatus.NOT_FOUND, "未找到请求的资源", requestPath));
+    }
+
+    // 其他情况，返回 SPA 首页内容，由前端处理 404
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE)
+        .body(getIndexHtml());
+  }
 
   /**
    * 处理自定义 API 异常。
@@ -242,5 +312,32 @@ public class GlobalExceptionHandler {
 
     // 以 ERROR 级别记录服务端异常
     log.error("服务端异常: {}", e.getMessage());
+  }
+
+  private static boolean isJsonRequest(final String originalPath, final String accept) {
+    return originalPath.startsWith(API_PATH_PREFIX)
+        || accept.contains(MediaType.APPLICATION_JSON_VALUE);
+  }
+
+  private String getIndexHtml() {
+    final Resource resource = resourceLoader.getResource(SPA_INDEX_PAGE);
+
+    if (!resource.exists()) {
+      throw new ApiException(
+          HttpStatus.INTERNAL_SERVER_ERROR,
+          "未找到 SPA 首页文件 [%s]".formatted(SPA_INDEX_PAGE)
+      );
+    }
+
+    try (
+        final InputStreamReader reader = new InputStreamReader(
+            resource.getInputStream(),
+            StandardCharsets.UTF_8
+        )
+    ) {
+      return FileCopyUtils.copyToString(reader);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
