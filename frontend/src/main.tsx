68c89fb89495db58409a50d0c2c15bb60fe78fbf
吴仙杰 <wuxianjiezh@gmail.com>
2023-11-29 18:40:28 +0800

    优化

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import { configureNProgress } from '@/shared/utils/nprogress'
 
+// 自定义样式放在最后，覆盖前面的样式
 import '@/index.css'
 
 configureNProgress()
