a8aaa8bc0f6a0fe8c475c8f04e46979cd4a928a4
吴仙杰 <wuxianjiezh@gmail.com>
2023-12-02 15:16:17 +0800

    优化 JavaDoc

--- backend/src/main/java/net/wuxianjie/web/version/Version.java
+++ backend/src/main/java/net/wuxianjie/web/version/Version.java
@@ -3,12 +3,12 @@ package net.wuxianjie.web.version;
 import java.time.LocalDateTime;
 
 /**
- * 项目版本信息。
+ * 项目版本号信息。
  *
- * @param name      项目名称
+ * @param name 项目名称
  * @param developer 项目开发者
- * @param version   项目版本号
- * @param builtAt   构建时间
+ * @param version 项目版本号
+ * @param builtAt 构建时间
  */
 public record Version(
   String name,
--- backend/src/main/java/net/wuxianjie/web/version/VersionController.java
+++ backend/src/main/java/net/wuxianjie/web/version/VersionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 
 /**
- * 版本信息。
+ * 版本号 API。
  */
 @RestController
 @RequestMapping("/api/v1")
@@ -16,31 +16,31 @@ public class VersionController {
   /**
    * 项目名称。
    */
-  public static final String NAME = "Spring Boot Demo";
+  private static final String NAME = "Spring Boot Demo";
 
   /**
    * 项目开发者。
    */
-  public static final String DEVELOPER = "吴仙杰";
+  private static final String DEVELOPER = "吴仙杰";
 
   /**
    * 项目版本号。
-   *
-   * <p>每次在生产环境中发布新包时都要更新版本号及构建时间。
+   * <p>
+   * 每次在生产环境中发布新包时都要更新版本号及构建时间。
    */
-  public static final String VERSION = "v1.0.0";
+  private static final String VERSION = "v1.0.0";
 
   /**
    * 项目构建时间。
-   *
-   * <p>每次在生产环境中发布新包时都要更新版本号及构建时间。
+   * <p>
+   * 每次在生产环境中发布新包时都要更新版本号及构建时间。
    */
-  public static final LocalDateTime BUILT_AT = LocalDateTime.of(2023, 11, 29, 13, 41, 56);
+  private static final LocalDateTime BUILT_AT = LocalDateTime.of(2023, 12, 2, 15, 14, 32);
 
   /**
    * 获取项目版本号。
-   *
-   * <p>每次在生产环境中发布新包时都要更新版本号及构建时间。
+   * <p>
+   * 每次在生产环境中发布新包时都要更新版本号及构建时间。
    */
   @GetMapping("/public/version")
   public Version getVersion() {
