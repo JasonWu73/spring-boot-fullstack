1c0f94c0eb0e04b29b1881061a07700dc301e0f9
吴仙杰 <wuxianjiezh@gmail.com>
2023-12-04 14:32:06 +0800

    优化链接样式

--- frontend/src/shared/components/ui/NotFoundPage.tsx
+++ frontend/src/shared/components/ui/NotFoundPage.tsx
@@ -4,7 +4,10 @@ export default function NotFoundPage() {
   return (
     <div className="mt-8 flex flex-col items-center gap-4">
       <h1>页面不存在 😞</h1>
-      <Link to="/" className="text-sky-500 hover:underline">
+      <Link
+        to="/"
+        className="text-sky-500 underline-offset-4 hover:text-sky-500/90 hover:underline"
+      >
         返回首页
       </Link>
     </div>
