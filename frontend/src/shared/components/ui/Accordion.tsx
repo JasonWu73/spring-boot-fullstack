29e1c06f9206102364bf4010bb961c85cd93bb1d
吴仙杰 <wuxianjiezh@gmail.com>
2023-12-05 10:16:57 +0800

    Accordion focus outline

--- frontend/src/shared/components/ui/Accordion.tsx
+++ frontend/src/shared/components/ui/Accordion.tsx
@@ -2,7 +2,7 @@ import * as AccordionPrimitive from '@radix-ui/react-accordion'
 import { ChevronDownIcon } from '@radix-ui/react-icons'
 import * as React from 'react'
 
-import { cn } from '@/shared/utils/helpers'
+import { cn, tw } from '@/shared/utils/helpers'
 
 const Accordion = AccordionPrimitive.Root
 
@@ -23,6 +23,7 @@ const AccordionTrigger = React.forwardRef<
       ref={ref}
       className={cn(
         'flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+        tw`focus-visible:rounded-md focus-visible:outline-none focus-visible:ring-1`,
         className
       )}
       {...props}
