9dcb2bfbb344f99b4ace7082694280a2be6596b7
吴仙杰 <wuxianjiezh@gmail.com>
2023-12-08 14:41:20 +0800

    调小一点加载点

--- frontend/src/shared/components/ui/Loading.tsx
+++ frontend/src/shared/components/ui/Loading.tsx
@@ -7,9 +7,9 @@ export function Loading() {
 
   return (
     <div className="flex items-center justify-center gap-2">
-      <div className="h-6 w-6 animate-bounce rounded-full bg-red-500 [animation-delay:-0.3s] dark:bg-red-600"></div>
-      <div className="h-6 w-6 animate-bounce rounded-full bg-amber-500 [animation-delay:-0.15s] dark:bg-amber-600"></div>
-      <div className="h-6 w-6 animate-bounce rounded-full bg-lime-500 dark:bg-lime-600"></div>
+      <div className="h-4 w-4 animate-bounce rounded-full bg-red-500 [animation-delay:-0.3s] dark:bg-red-600"></div>
+      <div className="h-4 w-4 animate-bounce rounded-full bg-amber-500 [animation-delay:-0.15s] dark:bg-amber-600"></div>
+      <div className="h-4 w-4 animate-bounce rounded-full bg-lime-500 dark:bg-lime-600"></div>
       <span className="sr-only">加载中...</span>
     </div>
   )
