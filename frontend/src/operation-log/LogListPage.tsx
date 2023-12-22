dbaebf62fae1f07b4701c8804d7f0facc29137f3
吴仙杰 <wuxianjiezh@gmail.com>
2023-12-22 11:09:20 +0800

    路由页要力求简洁

--- frontend/src/operation-log/LogListPage.tsx
+++ frontend/src/operation-log/LogListPage.tsx
@@ -1,9 +1,5 @@
-import { addDays, format } from 'date-fns'
-import { useSearchParams } from 'react-router-dom'
-
 import { LogSearch } from '@/operation-log/LogSearch'
 import { LogTable } from '@/operation-log/LogTable'
-import { getLogsApi, type GetLogsParams } from '@/shared/apis/backend/operation-log'
 import {
   Card,
   CardContent,
@@ -11,53 +7,10 @@ import {
   CardHeader,
   CardTitle
 } from '@/shared/components/ui/Card'
-import { DEFAULT_PAGE_NUM, DEFAULT_PAGE_SIZE } from '@/shared/components/ui/DataTable'
-import {
-  URL_QUERY_KEY_PAGE_NUM,
-  URL_QUERY_KEY_PAGE_SIZE,
-  URL_QUERY_KEY_SORT_COLUMN,
-  URL_QUERY_KEY_SORT_ORDER
-} from '@/shared/constants'
-import { useFetch } from '@/shared/hooks/use-fetch'
-import { useRefresh } from '@/shared/hooks/use-refresh'
 import { useTitle } from '@/shared/hooks/use-title'
 
 export default function LogListPage() {
   useTitle('操作日志')
-
-  const [searchParams] = useSearchParams()
-
-  const pageNum = Number(searchParams.get(URL_QUERY_KEY_PAGE_NUM)) || DEFAULT_PAGE_NUM
-  const pageSize = Number(searchParams.get(URL_QUERY_KEY_PAGE_SIZE)) || DEFAULT_PAGE_SIZE
-  const sortColumn = searchParams.get(URL_QUERY_KEY_SORT_COLUMN) || 'requestedAt'
-  const sortOrder = searchParams.get(URL_QUERY_KEY_SORT_ORDER) || 'desc'
-  const startAt =
-    searchParams.get('startAt') || format(addDays(new Date(), -6), 'yyyy-MM-dd')
-  const endAt = searchParams.get('endAt') || format(new Date(), 'yyyy-MM-dd')
-  const clientIp = searchParams.get('clientIp') || ''
-  const username = searchParams.get('username') || ''
-  const message = searchParams.get('message') || ''
-
-  const params: GetLogsParams = { pageNum, pageSize, startAt, endAt }
-
-  if (sortColumn) params.sortColumn = sortColumn
-  if (sortOrder) params.sortOrder = sortOrder === 'asc' ? 'asc' : 'desc'
-  if (startAt) params.startAt = startAt
-  if (endAt) params.endAt = endAt
-  if (clientIp) params.clientIp = clientIp
-  if (username) params.username = username
-  if (message) params.message = message
-
-  const {
-    loading: loadingLogs,
-    data: logs,
-    error: errorLogs,
-    fetchData: getLogs
-  } = useFetch(getLogsApi)
-
-  useRefresh(() => {
-    getLogs(params).then()
-  })
 
   return (
     <Card className="mx-auto h-full w-full">
@@ -69,16 +22,7 @@ export default function LogListPage() {
       <CardContent>
         <LogSearch />
 
-        <LogTable
-          data={logs?.list || []}
-          error={errorLogs}
-          loading={loadingLogs}
-          pagination={{
-            pageNum,
-            pageSize,
-            total: logs?.total || 0
-          }}
-        />
+        <LogTable />
       </CardContent>
     </Card>
   )
--- frontend/src/operation-log/LogTable.tsx
+++ frontend/src/operation-log/LogTable.tsx
@@ -1,7 +1,17 @@
 import type { ColumnDef, SortingState } from '@tanstack/react-table'
+import { addDays, format } from 'date-fns'
 
-import type { Log } from '@/shared/apis/backend/operation-log'
-import { DataTable, type Pagination } from '@/shared/components/ui/DataTable'
+import {
+  getLogsApi,
+  type GetLogsParams,
+  type Log
+} from '@/shared/apis/backend/operation-log'
+import {
+  DataTable,
+  DEFAULT_PAGE_NUM,
+  DEFAULT_PAGE_SIZE,
+  type Pagination
+} from '@/shared/components/ui/DataTable'
 import { DataTableColumnHeader } from '@/shared/components/ui/DataTableColumnHeader'
 import {
   URL_QUERY_KEY_PAGE_NUM,
@@ -9,14 +19,9 @@ import {
   URL_QUERY_KEY_SORT_COLUMN,
   URL_QUERY_KEY_SORT_ORDER
 } from '@/shared/constants'
+import { useFetch } from '@/shared/hooks/use-fetch'
+import { useRefresh } from '@/shared/hooks/use-refresh'
 import { useSearchParams } from 'react-router-dom'
-
-type OperationLogTableProps = {
-  data: Log[]
-  error?: string
-  loading?: boolean
-  pagination: Pagination
-}
 
 const columns: ColumnDef<Log>[] = [
   {
@@ -48,8 +53,35 @@ const columns: ColumnDef<Log>[] = [
   }
 ]
 
-export function LogTable({ data, error, loading, pagination }: OperationLogTableProps) {
+export function LogTable() {
   const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageNum = Number(searchParams.get(URL_QUERY_KEY_PAGE_NUM)) || DEFAULT_PAGE_NUM
+  const pageSize = Number(searchParams.get(URL_QUERY_KEY_PAGE_SIZE)) || DEFAULT_PAGE_SIZE
+  const sortColumn = searchParams.get(URL_QUERY_KEY_SORT_COLUMN) || 'requestedAt'
+  const sortOrder = searchParams.get(URL_QUERY_KEY_SORT_ORDER) || 'desc'
+  const startAt =
+    searchParams.get('startAt') || format(addDays(new Date(), -6), 'yyyy-MM-dd')
+  const endAt = searchParams.get('endAt') || format(new Date(), 'yyyy-MM-dd')
+  const clientIp = searchParams.get('clientIp') || ''
+  const username = searchParams.get('username') || ''
+  const message = searchParams.get('message') || ''
+
+  const params: GetLogsParams = { pageNum, pageSize, startAt, endAt }
+
+  if (sortColumn) params.sortColumn = sortColumn
+  if (sortOrder) params.sortOrder = sortOrder === 'asc' ? 'asc' : 'desc'
+  if (startAt) params.startAt = startAt
+  if (endAt) params.endAt = endAt
+  if (clientIp) params.clientIp = clientIp
+  if (username) params.username = username
+  if (message) params.message = message
+
+  const { loading, data: logs, error, fetchData: getLogs } = useFetch(getLogsApi)
+
+  useRefresh(() => {
+    getLogs(params).then()
+  })
 
   function handlePaginate(paging: Pagination) {
     searchParams.set(URL_QUERY_KEY_PAGE_NUM, String(paging.pageNum))
@@ -75,10 +107,14 @@ export function LogTable({ data, error, loading, pagination }: OperationLogTable
   return (
     <DataTable
       columns={columns}
-      data={data}
+      data={logs?.list || []}
       error={error}
       loading={loading}
-      pagination={pagination}
+      pagination={{
+        pageNum,
+        pageSize,
+        total: logs?.total || 0
+      }}
       onPaginate={handlePaginate}
       sortColumn={{
         id: '请求时间',
