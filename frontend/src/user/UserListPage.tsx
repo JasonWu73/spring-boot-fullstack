621b06be00ca3c2b9fbd40d7af48f8f0c2951e5e
吴仙杰 <wuxianjiezh@gmail.com>
2023-12-22 11:20:10 +0800

    路由页要力求简洁

--- frontend/src/user/UserListPage.tsx
+++ frontend/src/user/UserListPage.tsx
@@ -1,14 +1,3 @@
-import { useSignal } from '@preact/signals-react'
-import { useSearchParams } from 'react-router-dom'
-
-import {
-  deleteUserApi,
-  getUsersApi,
-  updateUserStatusApi,
-  type AccountStatus,
-  type GetUsersParams,
-  type User
-} from '@/shared/apis/backend/user'
 import {
   Card,
   CardContent,
@@ -16,153 +5,12 @@ import {
   CardHeader,
   CardTitle
 } from '@/shared/components/ui/Card'
-import { Code } from '@/shared/components/ui/Code'
-import { DEFAULT_PAGE_NUM, DEFAULT_PAGE_SIZE } from '@/shared/components/ui/DataTable'
-import { useToast } from '@/shared/components/ui/use-toast'
-import {
-  URL_QUERY_KEY_PAGE_NUM,
-  URL_QUERY_KEY_PAGE_SIZE,
-  URL_QUERY_KEY_SORT_COLUMN,
-  URL_QUERY_KEY_SORT_ORDER
-} from '@/shared/constants'
-import { useFetch } from '@/shared/hooks/use-fetch'
-import { useRefresh } from '@/shared/hooks/use-refresh'
 import { useTitle } from '@/shared/hooks/use-title'
 import { UserSearch } from '@/user/UserSearch'
 import { UserTable } from '@/user/UserTable'
 
-type UpdateUserStatus = {
-  userId: number
-  status: AccountStatus
-}
-
 export default function UserListPage() {
   useTitle('用户管理')
-
-  const [searchParams] = useSearchParams()
-
-  const pageNum = Number(searchParams.get(URL_QUERY_KEY_PAGE_NUM)) || DEFAULT_PAGE_NUM
-  const pageSize = Number(searchParams.get(URL_QUERY_KEY_PAGE_SIZE)) || DEFAULT_PAGE_SIZE
-  const sortColumn = searchParams.get(URL_QUERY_KEY_SORT_COLUMN) || 'createdAt'
-  const sortOrder = searchParams.get(URL_QUERY_KEY_SORT_ORDER) || 'desc'
-  const username = searchParams.get('username') || ''
-  const nickname = searchParams.get('nickname') || ''
-  const status = searchParams.get('status') || ''
-  const authority = searchParams.get('authority') || ''
-
-  const params: GetUsersParams = { pageNum, pageSize }
-
-  if (sortColumn) params.sortColumn = sortColumn
-  if (sortOrder) params.sortOrder = sortOrder === 'asc' ? 'asc' : 'desc'
-  if (username) params.username = username
-  if (nickname) params.nickname = nickname
-  if (status) params.status = status
-  if (authority) params.authority = authority
-
-  const {
-    loading: loadingUsers,
-    data: users,
-    error: errorUsers,
-    fetchData: getUsers,
-    invalidateFetch: invalidateUsers
-  } = useFetch(getUsersApi)
-
-  useRefresh(() => {
-    getUsers(params).then()
-  })
-
-  const { loading: loadingUpdateUserStatus, fetchData: updateUserStatus } = useFetch(
-    async ({ userId, status }: UpdateUserStatus) =>
-      await updateUserStatusApi(userId, status)
-  )
-
-  const { loading: loadingDeleteUser, fetchData: deleteUser } = useFetch(
-    async (userId: number) => await deleteUserApi(userId)
-  )
-
-  const indexes = useSignal<number[]>([]) // 选中的行的索引
-  const { toast } = useToast()
-
-  function handleShowSelection() {
-    const ids = (users?.list || [])
-      .filter((_, index) => indexes.value.includes(index))
-      .map((user) => user.id)
-
-    if (ids.length === 0) {
-      toast({
-        title: '未选中任何行',
-        description: '请先选中要操作的行',
-        variant: 'destructive'
-      })
-
-      return
-    }
-
-    toast({
-      title: '选中的行',
-      description: (
-        <span>
-          被选中的行 ID(s)：<Code>{ids.join(', ')}</Code>
-        </span>
-      )
-    })
-  }
-
-  async function handleChangeStatus(user: User, enabled: boolean) {
-    const newStatus = enabled ? 0 : 1
-
-    const { status, error } = await updateUserStatus({
-      userId: user.id,
-      status: newStatus
-    })
-
-    if (status !== 204) {
-      toast({
-        title: '更新账号状态失败',
-        description: error,
-        variant: 'destructive'
-      })
-
-      return
-    }
-
-    invalidateUsers().then()
-
-    toast({
-      title: '更新账号状态成功',
-      description: (
-        <span>
-          {!enabled ? '启用' : '禁用'} <Code>{user.username}</Code> 账号
-        </span>
-      )
-    })
-  }
-
-  async function handleDeleteUser(user: User) {
-    const { id, username } = user
-    const { status, error } = await deleteUser(id)
-
-    if (status !== 204) {
-      toast({
-        title: '删除用户失败',
-        description: error,
-        variant: 'destructive'
-      })
-
-      return
-    }
-
-    invalidateUsers().then()
-
-    toast({
-      title: '删除用户成功',
-      description: (
-        <span>
-          成功删除用户 <Code>{username}</Code>
-        </span>
-      )
-    })
-  }
 
   return (
     <Card className="mx-auto h-full w-full">
@@ -174,22 +22,7 @@ export default function UserListPage() {
       <CardContent>
         <UserSearch />
 
-        <UserTable
-          data={users?.list || []}
-          error={errorUsers}
-          loading={loadingUsers}
-          pagination={{
-            pageNum,
-            pageSize,
-            total: users?.total || 0
-          }}
-          submitting={loadingUpdateUserStatus || loadingDeleteUser}
-          onSelect={(rowIndexes) => (indexes.value = rowIndexes)}
-          onShowSelection={handleShowSelection}
-          onChangeStatus={handleChangeStatus}
-          onDeleteUser={handleDeleteUser}
-          invalidateUsers={invalidateUsers}
-        />
+        <UserTable />
       </CardContent>
     </Card>
   )
--- frontend/src/user/UserTable.tsx
+++ frontend/src/user/UserTable.tsx
@@ -2,50 +2,87 @@ import { useSignal } from '@preact/signals-react'
 import type { SortingState } from '@tanstack/react-table'
 import { Link, useSearchParams } from 'react-router-dom'
 
-import type { User } from '@/shared/apis/backend/user'
-import type { PaginationData } from '@/shared/apis/types'
+import {
+  deleteUserApi,
+  getUsersApi,
+  updateUserStatusApi,
+  type AccountStatus,
+  type GetUsersParams,
+  type User
+} from '@/shared/apis/backend/user'
 import { Button, buttonVariants } from '@/shared/components/ui/Button'
 import { Code } from '@/shared/components/ui/Code'
 import { ConfirmDialog } from '@/shared/components/ui/ConfirmDialog'
-import { DataTable, type Pagination } from '@/shared/components/ui/DataTable'
+import {
+  DEFAULT_PAGE_NUM,
+  DEFAULT_PAGE_SIZE,
+  DataTable,
+  type Pagination
+} from '@/shared/components/ui/DataTable'
+import { useToast } from '@/shared/components/ui/use-toast'
 import {
   URL_QUERY_KEY_PAGE_NUM,
   URL_QUERY_KEY_PAGE_SIZE,
   URL_QUERY_KEY_SORT_COLUMN,
   URL_QUERY_KEY_SORT_ORDER
 } from '@/shared/constants'
-import type { ApiResponse } from '@/shared/hooks/use-fetch'
+import { useFetch } from '@/shared/hooks/use-fetch'
+import { useRefresh } from '@/shared/hooks/use-refresh'
 import { hasRoot } from '@/shared/signals/auth'
 import { cn } from '@/shared/utils/helpers'
 import { ResetPasswordDialog } from '@/user/ResetPasswordDialog'
 import { getUserTableColumns } from '@/user/UserTableColumns'
 
-type UserTableProps = {
-  data: User[]
-  error?: string
-  loading?: boolean
-  pagination: Pagination
-  submitting: boolean
-  onSelect: (rowIndexes: number[]) => void
-  onShowSelection: () => void
-  onChangeStatus: (user: User, enabled: boolean) => void
-  onDeleteUser: (user: User) => void
-  invalidateUsers: () => Promise<ApiResponse<PaginationData<User>>>
+type UpdateUserStatus = {
+  userId: number
+  status: AccountStatus
 }
 
-export function UserTable({
-  data,
-  error,
-  loading,
-  pagination,
-  submitting,
-  onSelect,
-  onShowSelection,
-  onChangeStatus,
-  onDeleteUser,
-  invalidateUsers
-}: UserTableProps) {
+export function UserTable() {
   const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageNum = Number(searchParams.get(URL_QUERY_KEY_PAGE_NUM)) || DEFAULT_PAGE_NUM
+  const pageSize = Number(searchParams.get(URL_QUERY_KEY_PAGE_SIZE)) || DEFAULT_PAGE_SIZE
+  const sortColumn = searchParams.get(URL_QUERY_KEY_SORT_COLUMN) || 'createdAt'
+  const sortOrder = searchParams.get(URL_QUERY_KEY_SORT_ORDER) || 'desc'
+  const username = searchParams.get('username') || ''
+  const nickname = searchParams.get('nickname') || ''
+  const status = searchParams.get('status') || ''
+  const authority = searchParams.get('authority') || ''
+
+  const params: GetUsersParams = { pageNum, pageSize }
+
+  if (sortColumn) params.sortColumn = sortColumn
+  if (sortOrder) params.sortOrder = sortOrder === 'asc' ? 'asc' : 'desc'
+  if (username) params.username = username
+  if (nickname) params.nickname = nickname
+  if (status) params.status = status
+  if (authority) params.authority = authority
+
+  const {
+    loading: loadingUsers,
+    data: users,
+    error: errorUsers,
+    fetchData: getUsers,
+    invalidateFetch: invalidateUsers
+  } = useFetch(getUsersApi)
+
+  useRefresh(() => {
+    getUsers(params).then()
+  })
+
+  const { loading: loadingUpdateUserStatus, fetchData: updateUserStatus } = useFetch(
+    async ({ userId, status }: UpdateUserStatus) =>
+      await updateUserStatusApi(userId, status)
+  )
+
+  const { loading: loadingDeleteUser, fetchData: deleteUser } = useFetch(
+    async (userId: number) => await deleteUserApi(userId)
+  )
+
+  const indexes = useSignal<number[]>([]) // 选中的行的索引
+  const { toast } = useToast()
+
   const openDeleteDialog = useSignal(false)
   const openResetPasswordDialog = useSignal(false)
   const currentUser = useSignal<User | null>(null)
@@ -72,26 +109,107 @@ export function UserTable({
     setSearchParams(searchParams)
   }
 
-  function handleDeleteUser() {
-    if (currentUser.value) {
-      onDeleteUser(currentUser.value)
+  function handleShowSelection() {
+    const ids = (users?.list || [])
+      .filter((_, index) => indexes.value.includes(index))
+      .map((user) => user.id)
+
+    if (ids.length === 0) {
+      toast({
+        title: '未选中任何行',
+        description: '请先选中要操作的行',
+        variant: 'destructive'
+      })
+
+      return
     }
+
+    toast({
+      title: '选中的行',
+      description: (
+        <span>
+          被选中的行 ID(s)：<Code>{ids.join(', ')}</Code>
+        </span>
+      )
+    })
+  }
+
+  async function handleChangeStatus(user: User, enabled: boolean) {
+    const newStatus = enabled ? 0 : 1
+
+    const { status, error } = await updateUserStatus({
+      userId: user.id,
+      status: newStatus
+    })
+
+    if (status !== 204) {
+      toast({
+        title: '更新账号状态失败',
+        description: error,
+        variant: 'destructive'
+      })
+
+      return
+    }
+
+    invalidateUsers().then()
+
+    toast({
+      title: '更新账号状态成功',
+      description: (
+        <span>
+          {!enabled ? '启用' : '禁用'} <Code>{user.username}</Code> 账号
+        </span>
+      )
+    })
+  }
+
+  async function handleDeleteUser() {
+    if (!currentUser.value) return
+
+    const { id, username } = currentUser.value
+    const { status, error } = await deleteUser(id)
+
+    if (status !== 204) {
+      toast({
+        title: '删除用户失败',
+        description: error,
+        variant: 'destructive'
+      })
+
+      return
+    }
+
+    invalidateUsers().then()
+
+    toast({
+      title: '删除用户成功',
+      description: (
+        <span>
+          成功删除用户 <Code>{username}</Code>
+        </span>
+      )
+    })
   }
 
   return (
     <>
       <DataTable
         columns={getUserTableColumns({
-          submitting,
+          submitting: loadingUpdateUserStatus || loadingDeleteUser,
           currentUser,
-          onChangeStatus,
+          onChangeStatus: handleChangeStatus,
           openDeleteDialog,
           openResetPasswordDialog
         })}
-        data={data}
-        error={error}
-        loading={loading}
-        pagination={pagination}
+        data={users?.list || []}
+        error={errorUsers}
+        loading={loadingUsers}
+        pagination={{
+          pageNum,
+          pageSize,
+          total: users?.total || 0
+        }}
         onPaginate={handlePaginate}
         sortColumn={{
           id:
@@ -102,11 +220,11 @@ export function UserTable({
         }}
         onSorting={handleSorting}
         enableRowSelection
-        onSelect={onSelect}
+        onSelect={(rowIndexes) => (indexes.value = rowIndexes)}
       >
         {hasRoot() && (
           <div>
-            <Button variant="secondary" size="sm" onClick={onShowSelection}>
+            <Button variant="secondary" size="sm" onClick={handleShowSelection}>
               查看被选中的行
             </Button>
 
