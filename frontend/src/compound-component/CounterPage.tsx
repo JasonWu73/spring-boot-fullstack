84da4f739eb26897914450a339f62221abd6ed77
吴仙杰 <wuxianjiezh@gmail.com>
2023-12-22 11:36:54 +0800

    路由页要力求简洁

--- frontend/src/compound-component/CounterPage.tsx
+++ frontend/src/compound-component/CounterPage.tsx
@@ -1,6 +1,4 @@
-import { Minus, Plus } from 'lucide-react'
-
-import { Counter } from '@/compound-component/Counter'
+import { CompoundCounter } from '@/compound-component/CompoundCounter'
 import {
   Card,
   CardContent,
@@ -8,8 +6,6 @@ import {
   CardHeader,
   CardTitle
 } from '@/shared/components/ui/Card'
-import { Code } from '@/shared/components/ui/Code'
-import { Separator } from '@/shared/components/ui/Separator'
 import { useTitle } from '@/shared/hooks/use-title'
 
 export default function CounterPage() {
@@ -23,43 +19,7 @@ export default function CounterPage() {
       </CardHeader>
 
       <CardContent>
-        <Counter className="flex flex-col items-center justify-center gap-4">
-          <Counter.Label className="font-semibold">Counter 复合组件</Counter.Label>
-
-          <div className="flex items-center justify-center gap-2">
-            <Counter.Decrease icon={<Minus />} />
-            <Counter.Count />
-            <Counter.Increase icon={<Plus />} />
-          </div>
-        </Counter>
-
-        <Separator className="my-4" />
-
-        <Counter className="flex flex-col items-center justify-center gap-4">
-          <Counter.Label className="font-semibold">Counter 复合组件</Counter.Label>
-
-          <div className="flex items-center justify-center gap-2">
-            <Counter.Increase icon="▶️" />
-            <Counter.Count />
-            <Counter.Decrease icon="◀️" />
-          </div>
-        </Counter>
-
-        <Separator className="my-4" />
-
-        <Counter className="flex flex-col items-center justify-center gap-4">
-          <Counter.Label className="font-semibold">
-            <span>Counter 复合组件：</span>
-            <Code>
-              <Counter.Count />
-            </Code>
-          </Counter.Label>
-
-          <div className="flex items-center justify-center gap-2">
-            <Counter.Increase icon="🔜" className="dark:bg-slate-50" />
-            <Counter.Decrease icon="🔙" className="dark:bg-slate-50" />
-          </div>
-        </Counter>
+        <CompoundCounter />
       </CardContent>
     </Card>
   )
