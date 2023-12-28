e88741f1459c43a477c251dab5bb02ac50391fa6
吴仙杰 <wuxianjiezh@gmail.com>
2023-12-28 10:53:47 +0800

    重命名

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 
-import { router } from '@/Routes'
+import { router } from '@/routes'
 import { Toaster } from '@/shared/components/ui/Toaster'
 import { createAuthState } from '@/shared/auth/auth'
 import { createPanelFoldState } from '@/shared/components/layout/panel-fold/panel-fold'
