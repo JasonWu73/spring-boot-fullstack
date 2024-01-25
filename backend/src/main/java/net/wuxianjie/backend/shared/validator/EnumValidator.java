a08e4a74e996f1a3136aa906b10b91aecaf451e9
吴仙杰 <wuxianjiezh@gmail.com>
2024-01-25 13:30:21 +0800

    优化代码

--- backend/src/main/java/net/wuxianjie/backend/shared/validator/EnumValidator.java
+++ backend/src/main/java/net/wuxianjie/backend/shared/validator/EnumValidator.java
@@ -1,6 +1,11 @@
 package net.wuxianjie.backend.shared.validator;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
@@ -13,15 +18,16 @@ import net.wuxianjie.backend.shared.validator.impl.EnumValidatorImpl;
 /**
  * 枚举值验证注解，即用枚举类验证值是否合法。
  * <p>
- * 前提：作为 {@code value} 值的枚举类存在名为 {@value EnumValidatorImpl#METHOD_NAME}）的方法，且该方法的返回值就是需要验证的值。
+ * 前提：枚举类存在名为 {@value EnumValidatorImpl#METHOD_NAME} 的方法，且该方法的返回值就是需要验证的值。
  * <p>
- * 注意：{@code null} 值被认为是合法的。
+ * 注意：`null` 值被认为是合法的。
  **/
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = EnumValidatorImpl.class)
 @Repeatable(EnumValidator.List.class)
 public @interface EnumValidator {
+
   Class<? extends Enum<?>> value();
 
   String message() default "类型不合法";
@@ -33,6 +39,7 @@ public @interface EnumValidator {
   @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
   @Retention(RetentionPolicy.RUNTIME)
   @interface List {
+
     EnumValidator[] value();
   }
 }
--- backend/src/main/java/net/wuxianjie/backend/shared/validator/impl/EnumValidatorImpl.java
+++ backend/src/main/java/net/wuxianjie/backend/shared/validator/impl/EnumValidatorImpl.java
@@ -13,61 +13,40 @@ import net.wuxianjie.backend.shared.validator.EnumValidator;
  * 实现 {@link EnumValidator} 注解的验证逻辑。
  **/
 @Slf4j
-public class EnumValidatorImpl
-  implements ConstraintValidator<EnumValidator, Object> {
+public class EnumValidatorImpl implements ConstraintValidator<EnumValidator, Object> {
 
   /**
    * 枚举类中返回所对应验证值的方法名。
    */
   public static final String METHOD_NAME = "getCode";
 
-  private boolean isPassed;
+  private boolean passed;
   private List<Object> values;
 
   @Override
   public void initialize(final EnumValidator enumValidator) {
-    isPassed = false;
+    passed = false;
     values = new ArrayList<>();
+
     final Class<? extends Enum<?>> enumClass = enumValidator.value();
-
-    if (!enumClass.isEnum()) {
-      isPassed = true;
-      log.warn("忽略枚举值验证 [{} 不是枚举类]", enumClass.getName());
-      return;
-    }
-
     final Enum<?>[] enums = enumClass.getEnumConstants();
-
     for (final Enum<?> theEnum : enums) {
       try {
         final Method method = theEnum.getClass().getDeclaredMethod(METHOD_NAME);
         method.setAccessible(true);
         values.add(method.invoke(theEnum));
       } catch (NoSuchMethodException e) {
-        isPassed = true;
-
-        log.warn(
-          "忽略枚举值验证 [{} 不存在 {} 方法]",
-          enumClass.getName(),
-          METHOD_NAME
-        );
+        passed = true;
+        log.warn("忽略枚举值验证 [{} 不存在 {} 方法]", enumClass.getName(), METHOD_NAME);
       } catch (InvocationTargetException | IllegalAccessException e) {
-        isPassed = true;
-
-        log.warn(
-          "忽略枚举值验证 [{}.{} 方法执行出错]",
-          enumClass.getName(),
-          METHOD_NAME
-        );
+        passed = true;
+        log.warn("忽略枚举值验证 [{}.{} 方法执行出错]", enumClass.getName(), METHOD_NAME);
       }
     }
   }
 
   @Override
-  public boolean isValid(
-    final Object value,
-    final ConstraintValidatorContext context
-  ) {
-    return isPassed || value == null || values.contains(value);
+  public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+    return passed || value == null || values.contains(value);
   }
 }
