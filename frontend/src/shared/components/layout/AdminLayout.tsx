d8071c2e240cac280e46f82ffcc7c501edd1f957
吴仙杰 <wuxianjiezh@gmail.com>
2024-02-02 16:50:58 +0800

    代码布局代码

--- frontend/src/shared/components/layout/AdminLayout.tsx
+++ frontend/src/shared/components/layout/AdminLayout.tsx
@@ -4,26 +4,26 @@ import { Outlet } from 'react-router-dom'
 import { Header } from '@/shared/components/layout/Header'
 import { Aside } from '@/shared/components/layout/Aside'
 import { Footer } from '@/shared/components/layout/Footer'
-import { LoadingFullPage } from '@/shared/components/ui/LoadingFullPage'
 import { isCollapsed } from '@/shared/components/layout/side-menu-signals'
+import { LoadingFullPage } from '@/shared/components/ui/LoadingFullPage'
 import { cn } from '@/shared/utils/helpers'
 
 export function AdminLayout() {
-  const folded = isCollapsed()
+  const collapsed = isCollapsed()
 
   return (
-    <div className="grid h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr]">
+    <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] h-screen">
       <Header className="col-span-2 row-span-1"/>
 
-      <Aside className={cn('col-span-1 row-span-1', folded && 'hidden')}/>
+      <Aside className={cn('col-span-1 row-span-1', collapsed && 'hidden')}/>
 
       <main
         className={cn(
           'col-span-1 row-span-1 flex flex-col',
-          folded && 'col-span-2'
+          collapsed && 'col-span-2'
         )}
       >
-        <div className="relative flex-grow p-4">
+        <div className="flex-grow relative p-4">
           <React.Suspense fallback={<LoadingFullPage/>}>
             <Outlet/>
           </React.Suspense>
--- frontend/src/shared/components/layout/LoginLayout.tsx
+++ frontend/src/shared/components/layout/LoginLayout.tsx
@@ -7,13 +7,13 @@ import { LoadingFullPage } from '@/shared/components/ui/LoadingFullPage'
 export function LoginLayout() {
   return (
     <div className="flex flex-col h-screen">
-      <main className="relative flex-grow p-4 text-snow bg-one-dark bg-[url('/img/bg_login.png')] bg-[length:100%] bg-no-repeat">
+      <main className="flex-grow relative p-4 bg-one-dark bg-[url('/img/bg_login.png')] bg-[length:100%] bg-no-repeat">
         <React.Suspense fallback={<LoadingFullPage/>}>
           <Outlet/>
         </React.Suspense>
       </main>
 
-      <Footer className="text-slate-200 bg-one-dark"/>
+      <Footer className="text-slate-200 bg-slate-700"/>
     </div>
   )
 }
