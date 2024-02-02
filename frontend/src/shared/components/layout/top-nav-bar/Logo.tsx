3579fc0b0f0bd488ba4d889f1bd473d1a8cf4f9d
吴仙杰 <wuxianjiezh@gmail.com>
2024-02-03 04:27:09 +0800

    美化样式

--- frontend/src/shared/components/layout/top-nav-bar/Logo.tsx
+++ frontend/src/shared/components/layout/top-nav-bar/Logo.tsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom'
 
+import { cn } from '@/shared/utils/helpers'
+import { buttonVariantClasses } from '@/shared/components/ui/Button'
+
 export function Logo() {
   return (
     <Link
       to="/"
-      className="text-slate-200 focus:outline-none focus:rounded focus:ring-1 focus:ring-sky-500"
+      className={cn(
+        buttonVariantClasses('link'),
+        'px-2 text-slate-200 no-underline focus:text-slate-200 focus:ring-2'
+      )}
     >
-      <span className="flex items-center gap-2">
-        <h2
-          className="flex items-center h-8 pl-10 uppercase bg-[url('/img/react.svg')] bg-left bg-no-repeat"
-        >
-          React
-        </h2>
-      </span>
+      <h2
+        className="flex items-center h-8 pl-10 uppercase bg-[url('/img/react.svg')] bg-left bg-no-repeat"
+      >
+        React
+      </h2>
     </Link>
   )
 }
