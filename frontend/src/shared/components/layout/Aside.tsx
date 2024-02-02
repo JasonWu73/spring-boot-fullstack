afe950c1d112a232a42d60c03fe0b7640c1425d2
吴仙杰 <wuxianjiezh@gmail.com>
2024-02-02 17:52:27 +0800

    侧边栏菜单样式修正

--- frontend/src/shared/components/layout/Aside.tsx
+++ frontend/src/shared/components/layout/Aside.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react'
 
-import { SideNavbar } from "@/shared/components/layout/SideNavbar";
-import { cn } from "@/shared/utils/helpers";
+import { SideNavbar } from '@/shared/components/layout/SideNavbar'
+import { cn } from '@/shared/utils/helpers'
 
-type AsideProps = React.ComponentPropsWithoutRef<"header">;
+type AsideProps = React.ComponentPropsWithoutRef<'aside'>;
 
 export function Aside({ className, ...props }: AsideProps) {
   return (
     <aside
-      className={cn("text-slate-200 bg-one-dark-1 p-4", className)}
       {...props}
+      className={cn('text-slate-200 bg-one-dark-1 p-4', className)}
     >
-      <SideNavbar />
+      <SideNavbar/>
     </aside>
-  );
+  )
 }
