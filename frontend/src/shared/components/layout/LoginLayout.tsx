de06896dacee95228c56f6ccaa9e87e5fadf31c7
吴仙杰 <wuxianjiezh@gmail.com>
2024-02-04 10:18:37 +0800

    美化登录页

--- frontend/src/shared/components/layout/LoginLayout.tsx
+++ frontend/src/shared/components/layout/LoginLayout.tsx
@@ -6,14 +6,16 @@ import { LoadingFullPage } from '@/shared/components/ui/LoadingFullPage'
 
 export function LoginLayout() {
   return (
-    <div className="flex flex-col h-screen">
-      <main className="flex-grow relative p-4 bg-slate-50 bg-[url('/img/bg_login.png')] bg-[length:100%] bg-no-repeat dark:bg-one-dark">
+    <div className="flex flex-col h-screen bg-gradient-to-br from-blue-500 to-green-500 dark:bg-one-dark dark:bg-[url('/img/bg_login.png')] dark:bg-[length:100%] dark:bg-no-repeat">
+      <main
+        className="flex-grow relative p-4"
+      >
         <React.Suspense fallback={<LoadingFullPage/>}>
           <Outlet/>
         </React.Suspense>
       </main>
 
-      <Footer/>
+      <Footer className="bg-transparent dark:bg-transparent"/>
     </div>
   )
 }
