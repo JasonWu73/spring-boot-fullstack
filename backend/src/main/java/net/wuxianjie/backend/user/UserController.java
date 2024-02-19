0a2ce489c12ff4b45b29ba723b68b000014e3d79
吴仙杰 <wuxianjiezh@gmail.com>
2024-02-19 13:55:18 +0800

    增加 final 关键字

--- backend/src/main/java/net/wuxianjie/backend/user/UserController.java
+++ backend/src/main/java/net/wuxianjie/backend/user/UserController.java
@@ -103,7 +103,7 @@ public class UserController {
     final long userId = userService.addUser(param);
 
     // 构建新增资源的 URI
-    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+    final URI location = ServletUriComponentsBuilder.fromCurrentRequest()
       .path("/{userId}")
       .buildAndExpand(userId)
       .toUri();
